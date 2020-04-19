a53f54a812975cff2b85a58133deec613b7fcdba
ildar-mac <ildar.life@mail.ru>
2020-04-19 22:43:24 +0300

    start

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+use Model\User;
+
+function dd($var) {
+    echo '<pre style="margin-left: 100px; margin-top: 60px; background:#000; color: #00fe00; font-weight:bold; font-size: 14px">' . print_r($var, true) . '</pre>';
+}
+
+include '../vendor/autoload.php';
+
+

4e646deb4b5d94532ac07385f4c26721735bc95b
ildarminkrit <derorn@mail.ru>
2020-05-31 19:50:02 +0300

    add php 7.4

--- src/public/index.php
+++ src/public/index.php
@@ -1,11 +1,13 @@
 <?php
 
-use Model\User;
+$pd = new PDO("mysql:host=db;dbname=db", "root", "6");
 
 function dd($var) {
-    echo '<pre style="margin-left: 100px; margin-top: 60px; background:#000; color: #00fe00; font-weight:bold; font-size: 14px">' . print_r($var, true) . '</pre>';
+    echo '<pre style="margin-left: 100px; margin-top: 60px; position:absolute; padding: 10px; background:#000; color: #00fe00; font-weight:bold; font-size: 14px">' . print_r($var, true) . '</pre>';
 }
 
-include '../vendor/autoload.php';
+
+$a = $pd->query("SELECT * FROM `blo`")->fetchAll(2);
 
 
+dd($a);
